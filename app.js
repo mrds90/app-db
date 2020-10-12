1ffc6183f230949a15f0ef29c5ecfea2ebeca9fc
Marcos Dominguez <mrds0690@gmail.com>
2020-10-12 12:10:44 -0300

    conectar online la base de datos

--- app.js
+++ app.js
@@ -2,25 +2,39 @@ var express  = require("express"),
     app      = express(),
     http     = require("http"),
     server   = http.createServer(app),
-    mongoose = require('mongoose'); 
+    // io = require("socket.io").listen(server),
+    // npid = require("npid"),
+    // uuid = require('node-uuid'),
+    // _ = require('underscore')._,
+    bodyParser  = require('body-parser');
+
+    mongoose = require('mongoose');
     var cors = require('cors');
     var corsOptions = {origin:"*",optionSucessStatus:200};
     
-    
-  app.use(cors(corsOptions));
-  app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
+    app.set('port', process.env.PORT || 3000)
+    app.use(cors(corsOptions));
 
-app.get('/', function(req, res) {
+    var env = process.env.NODE_ENV || 'development';
+
+     var bodyParser = require('body-parser'); 
+     app.use(bodyParser.urlencoded({
+         extended: true
+     }));
+     app.use(bodyParser.json());
+  // app.use(express.methodOverride());
+  // app.use(app.mountpath);
+    
+   
+
+
+  app.get('/', function(req, res) {
   res.send("Hello world!");
 });
 
 routes = require('./routes/alumnos')(app);
 
-mongoose.connect('mongodb://localhost/alumnos', function(err, res) {
+mongoose.connect('mongodb+srv://myt:mata@cluster0.5dbpm.gcp.mongodb.net/aplicacion?retryWrites=true&w=majority', function(err, res) {
 	if(err) {
 		console.log('ERROR: connecting to Database. ' + err);
 	} else {
@@ -28,6 +42,6 @@ mongoose.connect('mongodb://localhost/alumnos', function(err, res) {
 	}
 });
 
-server.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+server.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
 });
